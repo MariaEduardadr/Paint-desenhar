4678b18319c3a388f23d58cddc1188ff549e23e4
MariaEduardadr <mariaeduarda.reis1404@gmail.com>
2023-10-20 00:06:46 -0300

    Coomit teste 2

--- script.js
+++ script.js
@@ -1,74 +1,62 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function (){
 
-})
-const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d")
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    const inputColor = document.querySelector(".input--color")
+    const tools = document.querySelectorAll(".button--tool");
+    const sizeButton = document.querySelectorAll(".button--size");
+    const buttonClear = document.querySelector(".button--clear");
+    
+    let brushSize = 20;
+    let isPainting = false
+    let activeTool = "brush"
+// desenhar
+    inputColor.addEventListener("change" , ({target}) => {
+        ctx.fillStyle = target.value;
+    })
 
-const inputColor = document.querySelector(".input--color")
-const tools = document.querySelectorAll(".button--tool")
-const sizeButtons = document.querySelectorAll(".button--size")
-const buttonClear = document.querySelector(".button--clear")
-
-let brushSize = 20
-
-let isPainting = false
-
-let activeTool = "brush"
-
-inputColor.addEventListener("change", ({ target }) => {
-    ctx.fillStyle = target.value
-})
-
-canvas.addEventListener("mousedown", ({ clientX, clientY }) => {
-    isPainting = true
-
-    if (activeTool == "brush") {
-        draw(clientX, clientY)
-    }
-
-    if (activeTool == "rubber") {
-        erase(clientX, clientY)
-    }
-})
-
-canvas.addEventListener("mousemove", ({ clientX, clientY }) => {
-    if (isPainting) {
-        if (activeTool == "brush") {
+    canvas.addEventListener("mousedown", (Event) => {
+        const{clientX, clientY} = Event
+        isPainting = true
+        if(activeTool == "brush"){
             draw(clientX, clientY)
         }
-
-        if (activeTool == "rubber") {
-            erase(clientX, clientY)
+        if(activeTool == "rubber"){
+            erase(clientX,clientY)
         }
+    })
+
+    canvas.addEventListener("mousemove", (Event) => {
+        const{clientX, clientY} = Event
+        if(isPainting){
+            if(activeTool == "brush"){
+                draw(clientX, clientY)
+            }
+        }
+        if(activeTool == "rubber"){
+            erase(clientX,clientY)
+        }
+    })
+    canvas.addEventListener("mouseup", (Event) => {
+      isPainting = false
+    })
+    const draw = (x, y) =>{
+        ctx.globalCompositeOperation = "source-over"
+        ctx.beginPath()
+        ctx.arc(x - canvas.offsetLeft,y - canvas.offsetTop , brushSize / 2, 0, 2 * Math.PI)
+        ctx.fill() 
     }
-})
+});
+// desenhar
 
-canvas.addEventListener("mouseup", ({ clientX, clientY }) => {
-    isPainting = false
-})
-
-const draw = (x, y) => {
-    ctx.globalCompositeOperation = "source-over"
-    ctx.beginPath()
-    ctx.arc(
-        x - canvas.offsetLeft,
-        y - canvas.offsetTop,
-        brushSize / 2,
-        0,
-        2 * Math.PI
-    )
-    ctx.fill()
-}
-
-const erase = (x, y) => {
+// apagar
+const erase = (x, y) =>{
     ctx.globalCompositeOperation = "destination-out"
     ctx.beginPath()
     ctx.arc(
         x - canvas.offsetLeft,
         y - canvas.offsetTop,
-        brushSize / 2,
-        0,
-        2 * Math.PI
+        0, 2 * Math.PI
     )
     ctx.fill()
 }
@@ -86,21 +74,21 @@ const selectTool = ({ target }) => {
 
 const selectSize = ({ target }) => {
     const selectedTool = target.closest("button")
-    const size = selectedTool.getAttribute("data-size")
+    const size = selectedTool.getAttribute("data--size")
 
     sizeButtons.forEach((tool) => tool.classList.remove("active"))
     selectedTool.classList.add("active")
     brushSize = size
+    sizeButtons.forEach((button) => {
+        button.addEventListener("click", selectSize)
+    })
+    
+    buttonClear.addEventListener("click", () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+    })
 }
-
+const tools = document.querySelectorAll(".button--tool");
 tools.forEach((tool) => {
     tool.addEventListener("click", selectTool)
 })
 
-sizeButtons.forEach((button) => {
-    button.addEventListener("click", selectSize)
-})
-
-buttonClear.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-})
